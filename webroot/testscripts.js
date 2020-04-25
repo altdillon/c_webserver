477658b42b5d937d1becf3c02f06570d497d8fa0
dillon <dmanhall@gmail.com>
2020-04-25 19:40:19 -0400

    updates to request demo page

--- webroot/testscripts.js
+++ webroot/testscripts.js
@@ -1,2 +1,35 @@
 
+(function(){
 
+  function ledOff(){
+    console.log("button off");
+    var xmlReq = new XMLHttpRequest();
+    xmlReq.open("GET","/ledstate?st=off",true);
+    xmlReq.send();
+  }
+
+  function ledOn(){
+    console.log("button on");
+    var xmlReq = new XMLHttpRequest();
+    xmlReq.open("GET","/ledstate?st=on",true);
+    xmlReq.send();
+  }
+
+  function getStatus(){
+  
+  }
+
+  window.onload = function() {
+    var button1 = document.getElementById("button1");
+    var button2 = document.getElementById("button2");
+    // attach button events
+    button1.onclick = function() {
+      ledOn();
+    }
+
+    button2.onclick = function() {
+      ledOff();
+    }
+  }
+
+})();
